282fa42136bc879304472c326be9f290bca94bfa
paulo-fs <jr.paulo87@gmail.com>
2023-05-14 10:01:25 -0300

    feat: add basic ong registration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
-
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 export const app = fastify()
+
+app.post('/ong', async (req, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password_hash: z.string().min(6),
+        whatsapp: z.string(),
+        city: z.string().max(50),
+        cep: z.string().max(8),
+        street: z.string().max(50).optional(),
+        district: z.string().max(50).optional()
+    })
+
+    const { name, email, password_hash, whatsapp, city, cep, district } = registerBodySchema.parse(req.body)
+
+    await prisma.oNG.create({
+        data: { name, email, password_hash, whatsapp, city, cep, district }
+    })
+
+    return reply.status(201).send()
+})
