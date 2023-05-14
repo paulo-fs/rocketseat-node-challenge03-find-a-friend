35c47678a5f666b5492e37c16a970765959da630
paulo-fs <jr.paulo87@gmail.com>
2023-05-14 16:56:30 -0300

    refactor: split code and add routes file

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,5 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
+import { appRoutes } from './http/routes'
 export const app = fastify()
 
-app.post('/ong', async (req, reply) => {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password_hash: z.string().min(6),
-        whatsapp: z.string(),
-        city: z.string().max(50),
-        cep: z.string().max(8),
-        street: z.string().max(50).optional(),
-        district: z.string().max(50).optional()
-    })
-
-    const { name, email, password_hash, whatsapp, city, cep, district } = registerBodySchema.parse(req.body)
-
-    await prisma.oNG.create({
-        data: { name, email, password_hash, whatsapp, city, cep, district }
-    })
-
-    return reply.status(201).send()
-})
+app.register(appRoutes)
