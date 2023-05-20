c3290128cb17718bfdd9c9bf7f892d63a53c8e2c
paulo-fs <jr.paulo87@gmail.com>
2023-05-20 19:15:00 -0300

    feat: implement ongs repository methods

--- src/repositories/prisma/prisma-ongs-repository.ts
+++ src/repositories/prisma/prisma-ongs-repository.ts
@@ -1,17 +1,23 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Ong, Prisma } from '@prisma/client'
 import { OngsRepository } from '../ongs-repository'
 
 export class PrismaOngsRepository implements OngsRepository{
-    async create(data: Prisma.ONGCreateInput) {
-        const ong = await prisma.oNG.create({
+
+    async create(data: Prisma.OngCreateInput) {
+        const ong = await prisma.ong.create({
             data
         })
         return ong
     }
 
+    async findByCity(city: string): Promise<Ong | null> {
+        const ong = await prisma.ong.findFirst({ where: { city }})
+        return ong
+    }
+
     async findByEmail(email: string) {
-        const ong = await prisma.oNG.findUnique({ where: { email }})
+        const ong = await prisma.ong.findUnique({ where: { email }})
         return ong
     }
 }
