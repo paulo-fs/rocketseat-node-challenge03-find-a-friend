f379ad0b0e474850c9aaf3c1b6e5378ce3293ad8
paulo-fs <jr.paulo87@gmail.com>
2023-05-21 18:30:41 -0300

    feat: add e2e tests for authenticate use case

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,36 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, beforeAll, afterAll, expect, it } from 'vitest'
 
-test('ok', () => {
-    console.log('teste')
+
+describe('Authenticate e2e', async () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register an ong', async () => {
+        await request(app.server).post('/ong').send({
+            name: 'ong test',
+            email: 'teste@example.com',
+            password: '123456',
+            whatsapp: '35999783000',
+            city: 'pouso alegre',
+            cep: '37550000',
+            street: 'teste'
+        })
+
+        const response = await request(app.server).post('/session').send({
+            email: 'teste@example.com',
+            password: '123456',
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String)
+        })
+    })
 })
