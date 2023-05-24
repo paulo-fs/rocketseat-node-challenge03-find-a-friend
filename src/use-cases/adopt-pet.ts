cca296338249ff549aa133c70ff0a6045b7a036a
paulo-fs <jr.paulo87@gmail.com>
2023-05-23 21:58:49 -0300

    fix: add update pet

--- src/use-cases/adopt-pet.ts
+++ src/use-cases/adopt-pet.ts
@@ -3,28 +3,16 @@ import { Pet } from '@prisma/client'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { PetAlreadyAdopted } from './errors/pet-already-adopted-error'
 
-interface AdoptPetUseCaseRequest {
-    petId: string
-}
-
-interface AdoptPetUseCaseResponse {
-    pet: Pet
-}
-
 export class AdoptPetUseCase {
     constructor(
         private petsRepository: PetsRepository
     ) {}
 
-    async execute(data: AdoptPetUseCaseRequest): Promise<AdoptPetUseCaseResponse> {
-        const { petId } = data
-
-        const pet = await this.petsRepository.findById(petId)
+    async execute(id: string): Promise<void> {
+        const pet = await this.petsRepository.findById(id)
         if (!pet) throw new ResourceNotFoundError()
         if(pet.adopted_at !== null) throw new PetAlreadyAdopted()
 
-        pet.adopted_at = new Date()
-
-        return { pet }
+        await this.petsRepository.adoptAPet(id)
     }
 }
